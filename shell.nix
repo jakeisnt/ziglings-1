b5d75a8690966e74e13fee1c02a9e2ef89ddb9c4
Benjamin Tan <benjamin@dev.ofcr.se>
2021-02-17 10:24:43 +0800

    zig-overlay: switch to flake
    Ref. https://github.com/arqv/zig-overlay/pull/3.

--- shell.nix
+++
@@ -1,9 +0,0 @@
-let
-  # TODO: switch back to fetchgit
-  # TODO: attempt to convert to flake?
-  zig-overlay = import ../zig-overlay/default.nix;
-  pkgs = import <nixpkgs> { overlays = [ zig-overlay ]; };
-  zig = pkgs.zig.master;
-in pkgs.mkShell {
-  buildInputs = [ zig ];
-}
